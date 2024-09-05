49e2e4099a2fa7815f42097384d5782da98f6482
aman <amanpatel22012003@gmail.com>
2024-09-05 22:15:17 +0530

    correct vercel error

--- src/app/page.js
+++ src/app/page.js
@@ -2,10 +2,12 @@ import Image from "next/image";
 import kv from "@vercel/kv";
 
 export default async function Home() {
-  await kv.set("user_1_session", "session_token_value");
-  console.time("kv");
-  const session = await kv.get("user_1_session");
-  console.timeEnd("kv");
+  // await kv.set("user_1_session", "session_token_value");
+  // console.time("kv");
+  // const session = await kv.get("user_1_session");
+  // console.timeEnd("kv");
 
-  return <div className="">{session}</div>;
+  return <div className="">
+    {/* {session} */}
+  </div>;
 }
